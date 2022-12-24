5ea8dc9291350c5468ff03ee9e4c0ffaf9492c4d
Sam Singer <sam.singer79@gmail.com>
2022-12-23 21:00:01 -0500

    finished did methods

--- pallets/schema-registry/src/lib.rs
+++ pallets/schema-registry/src/lib.rs
@@ -14,10 +14,11 @@ mod benchmarking;
 #[frame_support::pallet]
 pub mod pallet {
 
+	use crate::types::Registry;
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
-use sp_core::H256;
-	use crate::types::Registry;
+	use sp_core::H256;
+use types::VerifiableCredenialSchema;
 
 	#[pallet::pallet]
 	#[pallet::without_storage_info]
@@ -29,7 +30,6 @@ use sp_core::H256;
 	pub trait Config: frame_system::Config {
 		/// Because this pallet emits events, it depends on the runtime's definition of an event.
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
-		
 	}
 
 	// The pallet's runtime storage items.
@@ -37,7 +37,6 @@ use sp_core::H256;
 	#[pallet::getter(fn store_regisrty)]
 	pub type RegistryStore<T: Config> =
 		StorageMap<_, Blake2_128Concat, H256, Registry, OptionQuery>;
-
 
 	// Pallets use events to inform users when important changes are made.
 	// https://docs.substrate.io/main-docs/build/events-errors/
@@ -52,7 +51,6 @@ use sp_core::H256;
 		Updated(Registry),
 		// Event is emitted when an existing Registry item is deleted
 		Deleted(H256),
-
 	}
 
 	// Errors inform users that something went wrong.
@@ -67,7 +65,7 @@ use sp_core::H256;
 		///Error emitted when schema is unknown
 		UnknownSchema,
 		///Error emitted when schema is unknown
-		SchemaNotFound
+		SchemaNotFound,
 	}
 
 	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
@@ -91,9 +89,15 @@ use sp_core::H256;
 		}
 		// Function to update an existing schema
 		#[pallet::weight(10_000 + T::DbWeight::get().writes(1).ref_time())]
-		pub fn update(origin: OriginFor<T>, key: H256, old_data: Registry, new_data: Registry) -> DispatchResult {
+		pub fn update(
+			origin: OriginFor<T>,
+			key: H256,
+			old_data: Registry,
+			new_data: Registry,
+		) -> DispatchResult {
 			let _ = ensure_root(origin)?;
-			let mut onchain_data = RegistryStore::<T>::get(key.clone()).ok_or(Error::<T>::UnknownSchema)?;
+			let mut onchain_data =
+				RegistryStore::<T>::get(key.clone()).ok_or(Error::<T>::UnknownSchema)?;
 			ensure!(&old_data == &onchain_data, Error::<T>::SchemaNotFound);
 			onchain_data = new_data.clone();
 			// Save the updated schema vector in the storage
@@ -104,12 +108,47 @@ use sp_core::H256;
 
 		// Function to delete an existing schema
 		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1).ref_time())]
-        pub fn delete(origin: OriginFor<T>, key: H256) -> DispatchResult {
-            let _ = ensure_root(origin)?;
-            ensure!(<RegistryStore<T>>::contains_key(&key), Error::<T>::SchemaNotFound);
-            <RegistryStore<T>>::remove(&key);
-            Self::deposit_event(Event::Deleted(key));
-            Ok(())
-        }
+		pub fn delete(origin: OriginFor<T>, key: H256) -> DispatchResult {
+			let _ = ensure_root(origin)?;
+			ensure!(<RegistryStore<T>>::contains_key(&key), Error::<T>::SchemaNotFound);
+			<RegistryStore<T>>::remove(&key);
+			Self::deposit_event(Event::Deleted(key));
+			Ok(())
+		}
+		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1).ref_time())]
+		pub async fn create_public_did(
+			client: &Client<impl System>,
+			vc_credential: VerifiableCredenialSchema,
+		) -> Result<(), Box<dyn std::error::Error>> {
+			let call = Call::new(vc_credential.id, vc_credential);
+			let extrinsic = client.create_signed_extrinsic(call, None)?;
+			let hash = client.submit_extrinsic(extrinsic).await?;
+			println!("Data record created with ID {}", hash);
+			Ok(())
+		}
+		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1).ref_time())]
+		pub async fn update_public_did(
+			client: &Client<impl System>,
+			id: u64,
+			schema: String,
+			values: String,
+		) -> Result<(), Box<dyn std::error::Error>> {
+			let vc_credential = VerifiableCredenialSchema { id, schema, values };
+			let call = Call::new(id, vc_credential);
+			let extrinsic = client.create_signed_extrinsic(call, None)?;
+			let hash = client.submit_extrinsic(extrinsic).await?;
+			println!("Data record with ID {} updated", hash);
+			Ok(())
+		}
+		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1).ref_time())]
+		pub async fn delete_public_did(
+			client: &Client<impl System>,
+			id: u64,
+		) -> Result<(), Box<dyn std::error::Error>> {
+			let call = Call::new(id, ());
+			let extrinsic = client.create_signed_extrinsic(call, None)?;
+			let hash = client.submit_extrinsic(extrinsic).await?;
+			println!("Data record with ID {} deleted", hash);
+		}
 	}
 }
