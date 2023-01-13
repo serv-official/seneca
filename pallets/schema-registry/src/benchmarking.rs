3496ef629a06de8102e63a1f1be76980be76d1f3
ArnoldTumukunde <tumukunde@analog.one>
2023-01-13 14:26:09 +0300

    add benchmarking

--- pallets/schema-registry/src/benchmarking.rs
+++ pallets/schema-registry/src/benchmarking.rs
@@ -108,5 +108,45 @@ benchmarks! {
 	verify {
 		assert_eq!(SchemaRegistry::<T>::get(0), Some(schema));
 	}
+
+	delete_schema{
+		let s in 0 .. 100;
+	// Dispatch a signed extrinsic.
+	let name = b"Alice Data".to_vec();
+	let creator = b"did:serv:5HDx7jPsiED6n47eNfERrBBRHZb59jVW6UMZZMTSBpikzvhX".to_vec();
+	let expiration_date = Timestamp::now();
+	let mandatory_fields = Attribute{
+		name: b"name".to_vec(),
+		attribute_type: AttributeType::Hex,
+	};
+	let issuer_claims = Claim{
+		property: b"property".to_vec(),
+		value: b"value".to_vec(),
+		schema_id: None,
+		claim_type: ClaimType::IssuerClaim,
+	};
+	let subject_claims = Claim{
+		property: b"property".to_vec(),
+		value: b"value".to_vec(),
+		schema_id: None,
+		claim_type: ClaimType::SubjectClaim,
+	};
+	let credential_claims = Claim{
+		property: b"property".to_vec(),
+		value: b"value".to_vec(),
+		schema_id: None,
+		claim_type: ClaimType::CredentialClaim,
+	};
+	let account_pair = account_pair("Alice");
+	let account_pub = account_key("Alice");
+	// Encode and sign the schema message.
+	let data_sig = account_pair.sign(&creator);
+	assert_ok!(SchemaRegistry::create_schema(RawOrigin::Signed(account_pub).into(), 0, name, creator, 
+								vec![mandatory_fields], Some(expiration_date), issuer_claims, 
+								subject_claims, credential_claims, data_sig));
+	}:  _(RawOrigin::Signed(caller), 0)
+	verify {
+		assert_eq!(SchemaRegistry::<T>::get(0), None);
+	}
 	impl_benchmark_test_suite!(SchemaRegistry, crate::mock::new_test_ext(), crate::mock::Test);
 }
