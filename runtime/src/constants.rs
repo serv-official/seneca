5328b9ff56cffe1e533a2e1e867f292d04f9860b
ArnoldTumukunde <tumukunde@analog.one>
2023-01-20 16:41:51 +0300

    update token issuance

--- runtime/src/constants.rs
+++ runtime/src/constants.rs
@@ -4,9 +4,9 @@
 pub mod currency {
 	use node_primitives::Balance;
 
-	pub const MILLISER: Balance = 100_000_000;
-	pub const SER: Balance = MILLISER / 1_000; // assume this is worth about a cent.
-	pub const ZNO: Balance = SER / 100;
+	pub const MILLISER: Balance = 100_000;
+	pub const SER: Balance = 100 * MILLISER; // assume this is worth about a cent.
+	pub const ZNO: Balance = 10 * SER;
 
 	pub const fn deposit(items: u32, bytes: u32) -> Balance {
 		items as Balance * 15 * SER + (bytes as Balance) * 6 * SER
