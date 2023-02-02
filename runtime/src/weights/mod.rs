81a3aed7a1df4fc634d51a6fd7231ef5252e4f79
ArnoldTumukunde <tumukunde@analog.one>
2023-02-02 18:58:11 +0300

    add treasury weights

--- runtime/src/weights/mod.rs
+++ runtime/src/weights/mod.rs
@@ -8,6 +8,7 @@ pub mod pallet_membership;
 pub mod pallet_multisig;
 pub mod pallet_utility;
 pub mod frame_system;
+pub mod pallet_treasury;
 
 pub use block_weights::constants::BlockExecutionWeight;
 pub use extrinsic_weights::constants::ExtrinsicBaseWeight;
