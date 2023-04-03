d63898ceb77ddc7ab9fa0ee2642bc86e298c6f75
Sam Singer <samsinger@pop-os.localdomain>
2023-04-03 13:32:25 -0400

    Pallet is building

---
+++ pallets/offchain-worker/src/tests.rs
@@ -0,0 +1,7 @@
+use crate::mock::*;
+use crate::types::*;
+use codec::Encode;
+use frame_support::assert_ok;
+use frame_system::RawOrigin;
+use sp_core::Pair;
+use sp_runtime::traits::IdentifyAccount;
